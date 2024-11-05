4f4b6e930b52a9b895af312de10bffad00723613
EC2 Default User <ec2-user@ip-172-31-28-137.ap-northeast-1.compute.internal>
2024-11-05 10:53:45 +0000

    [update]showのルーティン未解決

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,12 @@
 class UsersController < ApplicationController
   def index
   end
+  def show
+    @user=User.find(params[:id])
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name)
+  end
 end
