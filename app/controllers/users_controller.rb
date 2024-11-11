f177504845b08432b4223e6aedc17e7b61456e49
EC2 Default User <ec2-user@ip-172-31-28-137.ap-northeast-1.compute.internal>
2024-11-11 15:15:27 +0000

    [update]ファイルの記述・CRUDの実装終わり

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,24 @@
 class UsersController < ApplicationController
   def index
+    @user=User.new
+    @users=User.all
   end
   def show
     @user=User.find(params[:id])
     @books = @user.books
   end
-
-
+  def update
+    @user = User.find(params[:id])
+    if  @user.update(user_params)
+         flash[:notice] = "User was successfully updated."
+         redirect_to user_path(@user.id)
+    else
+        @users=User.all
+        render :edit
+    end
+  end
   def edit
+    @user=User.find(params[:id])
   end
   private
   def user_params
